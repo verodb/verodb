b3e29ba0fead3088fb1e6edfd2a04ce921200bc8
abhishekbelgaonkar23 <155880197+abhishekbelgaonkar23@users.noreply.github.com>
2024-08-29 10:31:46 +0530

    cmake

---
+++ engine/build/CMakeFiles/verodb.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/verodb.dir/main.o"
+  "CMakeFiles/verodb.dir/main.o.d"
+  "CMakeFiles/verodb.dir/query.o"
+  "CMakeFiles/verodb.dir/query.o.d"
+  "CMakeFiles/verodb.dir/storage.o"
+  "CMakeFiles/verodb.dir/storage.o.d"
+  "verodb"
+  "verodb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/verodb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ engine/build/CMakeFiles/verodb.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for verodb.
+# This may be replaced when dependencies are built.
